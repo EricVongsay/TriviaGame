b4622912341a9d79fdaefa9502528756f83bc36c
Vongsy <evongsay@outlook.com>
2019-09-25 14:27:33 -0700

    added questions and answer array

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+// create variables for game start
+var timer=10;
+var correctAnswers=0;
+var wrongAnswers=0;
+
+var questions={
+    q1: "What year did Seattle lose the Super Sonics?",
+    q2: "Who was the leading scorer for the last two years? (2017-2019)",
+    q3: "Who leads the league in assists for the 2018-2019 season?",
+    q4: "How many championships does Michael Jordan have?",
+    q5: "How many championships does Lebron James have?",
+    q6: "How many championships does Kobe Bryant have?",
+    q7: "How many NBA teams are there?",
+    q8: "Who leads the league in all-time scoring?"
+}
+
+var choices={
+    q1:["2005","2010","2008","2009"],
+    q2:["James Harden","Lebron James","Russel Westbrook","Kevin Durant"],
+    q3:["Trae Young","Russel Westbrook","Stephen Curry","Chris Paul"],
+    q4:[3,5,7,6],
+    q5:[3,2,4,6],
+    q6:[3,6,4,5],
+    q7:[20,25,30,35],
+    q8:["Kobe Bryant","Lebron James","Karl Malone","Kareem Abdul-Jabbar"]
+}
+
+var answers = {
+    q1: "2008",
+    q2: "James Harden",
+    q3: "Russel Westbrook",
+    q4: 6,
+    q5: 3,
+    q6: 5,
+    q7: 30,
+    q8: "Kareem Abdul-Jabbar"
+}
+// loop through array of questions with a timer for each question
+// if answer selected show correct or wrong answer screen on timer
+//  
+
+
+
+
+
+
+
+
+
+
+
+
+});
